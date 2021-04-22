8a006e4e5b9d95eedf645268cb0c03589e77dbf7
CAN BERK ÖCALIR <can@ocalir.dev>
2021-04-22 00:06:57 +0000

    Adding final blockchain structure

--- main.py
+++ main.py
@@ -9,4 +9,21 @@ class Block:
 
 class Blockchain:
   def __init__(self):
-    self.chain = []
+    core = Block('gen_data', 'gen_hash', 'gen_last_hash')
+
+    self.chain = [core]
+
+  def add_block(self, data):
+      last_hash = self.chain[-1].hash
+      hash = lightning_hash(data + last_hash)
+      block = Block(data, hash, last_hash)
+
+      self.chain.append(block)
+
+foo_blockchain = Blockchain()
+foo_blockchain.add_block('one')
+foo_blockchain.add_block('two')
+foo_blockchain.add_block('three')
+
+for block in foo_blockchain.chain:
+    print(block.__dict__)
